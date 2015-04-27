ef9751f0ba1938591bbdb01a8571e6c72f33b404
Alastair Tse <altse@google.com>
2015-04-26 21:35:53 -0400

    Adding WebViewScreenSaverRig target
    Creating a target for hosting screensaver parts that can be tested without having to load it in to System Preferences.
    
    Conflicts:
    	WebViewScreenSaver/WebViewScreenSaver.xcodeproj/project.pbxproj
    	WebViewScreenSaver/WebViewScreenSaverRig/AppDelegate.m

---
+++ WebViewScreenSaver/WebViewScreenSaverRig/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WebViewScreenSaverRig
+//
+//  Created by Alastair Tse on 26/04/2015.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ WebViewScreenSaver/WebViewScreenSaverRig/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  WebViewScreenSaverRig
+//
+//  Created by Alastair Tse on 26/04/2015.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}
